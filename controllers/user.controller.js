28682660bc2a9fc26ddfd16b7703c43db1b9ea0e
MubashirASE <mubashir.sohail@codefinitysol.com>
2025-10-30 14:20:17 +0500

    first commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,6 @@
+import { User } from "../models/user.model.js";
+
+
+export const signup =(req,res)=>{
+    console.log(req.body)
+} 
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+import userRouter from "./routes/user.route.js"
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+
+app.use(cors());
+
+
+app.use("/api/v1/user",userRouter);
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
